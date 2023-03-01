08d1f0b9a7301ab191bc591baada1ca3d9605456
CoderZ <adzohonouapelete@gmail.com>
2023-03-01 11:37:51 +0000

    quote requet done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
 import gsap from "gsap";
+import { useEffect, useState } from "react";
 import Helmet from "react-helmet"
 import "./App.css"
 
 function App() {
 
+  // Current quote variables ...
+  let firstInit;
+  const [currentQuote, setCurrentQuote] = useState(null)
+
   // mini-function to generate random numbers
   // this is an utils of gsap
   const random = (a, b) => { return gsap.utils.random(a, b, 1) }
 
-  // Function to change the background with animation
+  // first time load
+  useEffect(() => { NewQuote() }, [firstInit])
+
+
+  // Function to change the background with animation &
   // and request to get new quote
-  const NewQuote = (e) => {
-    e.preventDefault()
+  const NewQuote = async () => {
+
     // Differents timelines initialization for the two animations
     const TL = gsap.timeline()
     const TL2 = gsap.timeline()
 
-    // Generation colors with the gsap random utility function
-    const primaryColor = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`
-
     // Vanishing all the contents the #quote-box container
     TL.to("#quote-box *", { opacity: 0, duration: .3 })
 
-    // Provisoiry timeout to simulate the animations
-    setTimeout(() => {
+    // getting the new quote
+    var myHeaders = new Headers();
+    myHeaders.append("x-api-key", "dqB2c4TR3nebdrhkFYBUuw==ZxSVuV2Qi2tLRCV2");
+
+    // requestOptions
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    const newQuote = await fetch("https://api.api-ninjas.com/v1/quotes", requestOptions)
+      .then(response => response.json())
+
+    if (newQuote !== null) {
+
+      // Generation colors with the gsap random utility function
+      const primaryColor = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`
+
+      // setting the new quote
+      setCurrentQuote(newQuote[0])
+
       TL2
         .to(["#quote-box *"], { opacity: 1, duration: .5 }, 1)
         .to(["#root", ".bar a"], {
@@ -32,7 +58,7 @@ function App() {
         .to("#quote-box", {
           color: primaryColor, duration: .3
         }, -1)
-    }, 50)
+    }
   }
 
   return (
@@ -50,31 +76,30 @@ function App() {
       <div id="quote-box">
         <div id="text">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, enim molestias.
+            {currentQuote?.quote}
           </p>
         </div>
         <div id="author">
-          author
+          {currentQuote?.author}
         </div>
         <div className="bar">
           <a
             id="new-quote"
-
             onClick={(e) => NewQuote(e)}
           >
             New quote
           </a>
           <a
             id="tweet-quote"
-            href="https://twitter.com/intent/tweet"
+            href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="${currentQuote?.quote} ${currentQuote?.author}`}
             title="Tweet this quote!"
-            target="_top"
-            onClick={e => e.preventDefault()}
+            target="_blank"
           >
-            <i class="fa fa-twitter"></i>
+            <i className="fa fa-twitter"></i>
           </a>
         </div>
       </div>
+
       {/* End Quote box & children */}
 
       <p id="me">by Apelete ADZOHONOU</p> {/* The name of the constructor.....ME */}
